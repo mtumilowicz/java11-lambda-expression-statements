455ae07fb06e002af118364b61f3f4ddd47b0a3d
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-12-28 23:19:29 +0100

    expression statements as lambda - tests

---
+++ src/test/java/LambdaStatementTest.java
@@ -0,0 +1,49 @@
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
+
+/**
+ * Created by mtumilowicz on 2018-12-28.
+ */
+public class LambdaStatementTest {
+
+    private int x = 5;
+    
+    @Test
+    public void increment() {
+        Consumer<String> increment = s -> x++;
+
+        increment.accept("a");
+        increment.accept("a");
+        increment.accept("a");
+        increment.accept("a");
+        
+        assertThat(x, is(9));
+    }
+    
+    @Test
+    public void add_to_list() {
+        var list = new LinkedList<String>();
+        Consumer<String> add = list::add;
+        
+        add.accept("a");
+        
+        assertThat(list, hasSize(1));
+    }
+    
+    @Test
+    public void add_in_foreach() {
+        var source = List.of(1, 2, 3);
+        var target = new LinkedList<Integer>();
+        
+        source.forEach(target::add);
+        
+        assertThat(target, is(source));
+    }
+}
